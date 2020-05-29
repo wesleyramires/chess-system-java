cdd5dc06a5c9789eff8f05f6bef21526238a2ee2
Wesley Ramires <ramirescristao@gmail.com>
2020-05-29 12:23:41 -0300

    Pawn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -20,7 +20,6 @@ public class Rook extends ChessPiece{
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
-		
 		Position p = new Position(0,0);
 		
 		//Above
